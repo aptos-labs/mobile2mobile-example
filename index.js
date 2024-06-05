a214a5ae4dce05a03d238ddecc76377956d5f2ae
maykonmichel <maykonmichelpalma@gmail.com>
2024-06-05 10:25:50 -0300

    chore(deps): install buffer react-native-get-random-values react-native-url-polyfill tweetnacl
    setup react-native-get-random-values react-native-url-polyfill/auto

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
  * @format
  */
 
+import 'react-native-get-random-values';
+import 'react-native-url-polyfill/auto';
+
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
