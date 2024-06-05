069812a9ecbbab39c23c658bbb88f129638f3a4a
maykonmichel <maykonmichelpalma@gmail.com>
2024-06-05 10:23:10 -0300

    chore: setup deeplink scheme to mobile2mobile-example

--- ios/Mobile2MobileExample/AppDelegate.mm
+++ ios/Mobile2MobileExample/AppDelegate.mm
@@ -2,6 +2,8 @@
 
 #import <React/RCTBundleURLProvider.h>
 
+#import <React/RCTLinkingManager.h>
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
@@ -26,6 +28,13 @@
 #else
   return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 #endif
+}
+
+- (BOOL)application:(UIApplication *)application
+   openURL:(NSURL *)url
+   options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options
+{
+  return [RCTLinkingManager application:application openURL:url options:options];
 }
 
 @end
